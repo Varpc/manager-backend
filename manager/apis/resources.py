72f57c2f331c19055c49320c74a09f3a825a4ff8
LML <704445755@qq.com>
2019-01-20 15:01:08 +0800

    update .gitignore

--- manager/apis/resources.py
+++ manager/apis/resources.py
@@ -8,7 +8,7 @@ from manager.apis.schemas import problems_schema, user_schema, jisuanke_schema, 
 from manager.apis.error import api_abort
 from manager.apis.auth import generate_token, validate_token
 from manager.extensions import db
-from manager.models import User, Problems, JiSuanKe, Codeforces, Root
+from manager.models import User, Problems, JiSuanKe, Codeforces, Root, Image
 
 
 class ProblemsApi(MethodView):
@@ -100,6 +100,50 @@ class EditerMediaApi(MethodView):
             file.save(filepath)
             return current_app.config['IMAGE_DIR_SUFFIX'] + filename, 200
         return api_abort(400)
+
+
+# 用于接收首页图片的修改提交
+class HomeImageApi(MethodView):
+    def get(self):
+        root = Root.query.first()
+        images = Image.query.with_parent(root).all()
+        image_list = []
+        for item in images:
+            filename = item.url.split('/')[-1:][0]
+            image_list.append(dict(imgURL=item.url, name=filename))
+        return jsonify(data=image_list)
+
+    def post(self):
+        file = request.files['file']
+        if file:
+            filename = str(uuid.uuid1()) + file.filename
+            filepath = os.path.join(current_app.config['IMAGE_DIR'], filename)
+            file.save(filepath)
+            image_url = current_app.config['IMAGE_DIR_SUFFIX'] + filename
+            root = Root.query.first()
+            image = Image(url=image_url)
+            root.image.append(image)
+            db.session.add(image)
+            db.session.commit()
+            return jsonify(code="0", imgURL=image_url, downloadURL=image_url)
+        return api_abort(400)
+
+    def delete(self):
+        data = request.get_json()
+        if data is None:
+            return api_abort(400)
+        image_url = data.get('imgURL')
+        if image_url is None:
+            return api_abort(400)
+        filename = image_url.split('/\\')[-1:][0]
+        filepath = current_app.config['IMAGE_DIR'] + filename
+        if os.path.exists(filepath):
+            os.remove(filepath)
+        image = Image.query.filter_by(url=image_url).first()
+        if image is not None:
+            db.session.delete(image)
+            db.session.commit()
+        return api_abort(200)
 
 
 # 近期比赛api
