930f7b12dd88ca621fa4f568cf66d7519969b32a
LML <704445755@qq.com>
2019-02-18 22:37:04 +0800

    部署

--- app.py
+++ app.py
@@ -2,17 +2,12 @@
 
 import threading
 import time
-from manager import create_app
 from crawler_manager import crawler_manager
 
 port = 5000
 
 if __name__ == '__main__':
-    manager = create_app()
-    manager_thread = threading.Thread(target=manager.run, kwargs=dict(port=port))
     crawler_thread = threading.Thread(target=crawler_manager, args=(port,))
-    manager_thread.start()
     time.sleep(10)
     crawler_thread.start()
-    manager_thread.join()
     crawler_thread.join()
